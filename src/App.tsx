46e58c570734074ce6dd5269e4f1b1449005f624
Eric Miranda <ericmiranda7@gmail.com>
2023-03-11 06:19:05 +0000

    update import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Home from './components/Home';
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
 import STest from './components/STest';
 
 export default function App() {
