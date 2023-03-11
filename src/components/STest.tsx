e055a40abe347f5bbbd11a0d51f4ad7c0041a708
Eric Miranda <ericmiranda7@gmail.com>
2023-03-11 06:15:08 +0000

    final commit

--- src/components/STest.tsx
+++ src/components/STest.tsx
@@ -3,16 +3,19 @@ import { useEffect, useState } from 'react';
 export default function STest() {
   const [msg, setMsg] = useState('');
   const [ws, setWs] = useState<WebSocket | null>(null);
+  console.log('rerendad');
 
   useEffect(() => {
-    setWs(new WebSocket('ws://127.0.0.1:8080/ws'));
-  }, []);
-
-  const handleClick = () => {
-    ws?.addEventListener('message', (event) => {
+    console.log('made a ws connection');
+    const tempWs = new WebSocket('ws://127.0.0.1:8080/ws');
+    tempWs?.addEventListener('message', (event) => {
       console.log('server said: ', event.data);
       setMsg(event.data);
     });
+    setWs(tempWs);
+  }, []);
+
+  const handleClick = () => {
     ws?.send('hi there');
   };
 
